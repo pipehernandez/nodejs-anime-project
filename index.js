fcf2bef2ecc2d581df8ffceaeda29819e5d47670
pipehernandez <90816576+pipehernandez@users.noreply.github.com>
2024-07-19 20:16:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 import "./src/app.js";
+
+// a partir de la version 20
+// para "reemplazar" nodemon: node --watch index.js
+// para "reemplazar" dotenv: node --env-file=.env index.js
+// ambos: node --env-file=.env --watch index
